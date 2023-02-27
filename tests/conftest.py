c0ca7c25cd6a478c9f705792ff97bacdd9484d72
William Fong <willfong@mit.edu>
2023-02-27 15:46:29 -0700

    setting up database fixture

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ import os
 
 import psycopg2
 import pytest
+from psycopg2.errors import DuplicateDatabase
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
@@ -13,38 +15,67 @@ from alembic.config import Config
 
 @pytest.fixture(scope="module")
 def database():
-    user = os.environ["username"]
-    password = os.environ["password"]
-    host = os.environ["dastabase_host"]
-    port = os.environ["database_port"]
+    kwargs = {}
+    with open(".test_env", "rt") as fin:
+        for line in fin:
+            k, v = line.strip().split("=")
+            kwargs[k] = v
 
-    conn = psycopg2.connect(user=user, host=host, password=password)
+    user = kwargs["username"]
+    password = kwargs["password"]
+    host = kwargs["database_host"]
+    port = kwargs["database_port"]
+
+    conn = psycopg2.connect(
+        dbname="postgres",
+        user=user,
+        host=host,
+        password=password,
+    )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     database_name = f"dpt_testing_{os.getpid()}"
 
-    with conn.cursor() as cur:
-        cur.execute(f"CREATE DATABASE {database_name}")
-        conn.commit()
+    try:
+        with conn.cursor() as cur:
+            cur.execute(f"CREATE DATABASE {database_name}")
+    except DuplicateDatabase:
+        conn.set_session(autocommit=True)
+        with conn.cursor() as cur:
+            cur.execute(f"DROP DATABASE {database_name}")
+            cur.execute(f"CREATE DATABASE {database_name}")
 
     conn = psycopg2.connect(
         dbname=database_name, user=user, host=host, password=password
     )
 
-    config = Config("./alembic.ini")
+    with open("./alembic.ini", "rt") as fin, open(
+        "./alembic.ini.conv", "wt"
+    ) as fout:
+        for line in fin:
+            if line.startswith("sqlalchemy.url"):
+                url = (
+                    f"postgresql://{user}:{password}@{host}"
+                    f":{port}/{database_name}"
+                )
+                fout.write(f"sqlalchemy.url = {url}\n")
+            else:
+                fout.write(line)
+    config = Config("./alembic.ini.conv")
     config.attributes["connection"] = conn
 
     command.upgrade(config, "head")
     conn.close()
 
     engine = create_engine(
-        f"psycopg2://{user}:{password}@{host}:{port}/{database_name}"
+        f"postgresql://{user}:{password}@{host}:{port}/{database_name}"
     )
     sessionmaker(bind=engine)
     yield sessionmaker
 
     conn = psycopg2.connect(
-        dbname=database_name, user=user, host=host, password=password
+        dbname="postgres", user=user, host=host, password=password
     )
+    conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
     with conn.cursor() as cur:
         cur.execute(f"DROP DATABASE {database_name}")
-        cur.commit()
     conn.close()
