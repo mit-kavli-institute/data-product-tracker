2d1d281f84f6ea06a04b9403cfcc59b5e6203a8e
William Fong <willfong@mit.edu>
2023-05-03 11:39:38 -0400

    bumping version

--- data_product_tracker/__init__.py
+++ data_product_tracker/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """William Christopher Fong"""
 __email__ = "willfong@mit.edu"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 
 from data_product_tracker import sql as _sql  # noqa F401
